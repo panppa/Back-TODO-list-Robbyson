9d960428ae53fbba16b336aff6f575f6bf8d1d7e
Gabriel Campos <gabrielfcampos2017@gmail.com>
2024-04-12 17:18:19 -0300

    mudando o req on data para req.body

--- src/app.js
+++ src/app.js
@@ -3,25 +3,27 @@ import mongoose, { mongo } from "mongoose"
 import routes from "./routes"
 import cors from "cors"
 
-class App{
-    constructor(){
+class App {
+    constructor() {
         this.server = express()
         this.database()
         this.cors()
+        this.server.use(express.urlencoded({ extended: true }));
+        this.server.use(express.json())
         this.server.use(routes)
-        
+
 
     }
-    database (){
-        console.log("mongao")
+    database() {
         mongoose.connect('mongodb+srv://gabrielfcampos2017:K06j8mNHqhhTORxa@cluster0.mfcidsv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+        console.log("conectado no mongao")
     }
-    cors (){
-        const corsOptions ={
-            origin:'*', 
-            credentials:true
-         }
-        this.server.use(cors(corsOptions)) 
+    cors() {
+        const corsOptions = {
+            origin: '*',
+            credentials: true
+        }
+        this.server.use(cors(corsOptions))
     }
 }
 export default new App().server
--- src/controllers/TarefaController.js
+++ src/controllers/TarefaController.js
@@ -1,57 +1,51 @@
 import Tarefa from "../schemas/Tarefa"
 
-class TarefaController{
-    constructor(){
-        
+class TarefaController {
+    constructor() {
     }
-    create(req,res){
-        req.on("data", async (data) => {
-            const dataTarefa = JSON.parse(data)
-            await Tarefa.create(dataTarefa)
-            return res.json(dataTarefa)
-        })
+
+    async create(req, res) {
+        const dataTarefa = req.body
+        await Tarefa.create(dataTarefa)
+        return res.json(dataTarefa)
     }
-    
-    update(req,res){
-        req.on("data", async (data) => 
-        {
-            const dataTarefa = JSON.parse(data)
-            const { id } = req.params
-            const tarefas = await Tarefa.findOneAndUpdate({"_id" : {id}.id},{description: dataTarefa.description, duedate: dataTarefa.duedate, done: dataTarefa.done, hide: dataTarefa.hide})
-            return res.json(tarefas)
-        })
+
+    async update(req, res) {
+        const dataTarefa = req.body
+        const { id } = req.params
+        const tarefas = await Tarefa.findOneAndUpdate({ "_id": { id }.id }, { description: dataTarefa.description, duedate: dataTarefa.duedate, done: dataTarefa.done, hide: dataTarefa.hide })
+        return res.json(tarefas)
     }
-    async read(req,res){
-        const sortDate = { duedate: 1}
+
+    async read(req, res) {
+        const sortDate = { duedate: 1 }
         const tarefas = await Tarefa.find().sort(sortDate)
         return res.json(tarefas)
 
     }
-    async delete(req,res){
+    async delete(req, res) {
         const { id } = req.params
         console.log("chegou aqui")
-        const tarefas = await Tarefa.deleteOne({"_id" : {id}.id})
-        return console.log(tarefas," Deletado")
-    
+        const tarefas = await Tarefa.deleteOne({ "_id": { id }.id })
+        return console.log(tarefas, " Deletado")
+
     }
-    async readId(req,res){
+    async readId(req, res) {
         const { id } = req.params
-        const tarefas = await Tarefa.find({"_id" : {id}.id})
+        const tarefas = await Tarefa.find({ "_id": { id }.id })
         return res.json(tarefas[0])
     }
- hide(req,res){
-    req.on("data", async (data) => 
-    {
-        const dataTarefa = JSON.parse(data)
+    async hide(req, res) {
+        const dataTarefa = req.body
         const { id } = req.params
-        const tarefa = await Tarefa.find({"_id" : {id}.id})
-        console.log(tarefa[0].done)
-        if (tarefa[0].done){
-           const tarefas = await Tarefa.findOneAndUpdate({"_id" : id},{description: dataTarefa.description, duedate: dataTarefa.duedate, done: dataTarefa.done, hide: dataTarefa.hide})
+        const tarefa = await Tarefa.find({ "_id": { id }.id })
+        if (tarefa[0].done) {
+            const tarefas = await Tarefa.findOneAndUpdate({ "_id": id }, { description: dataTarefa.description, duedate: dataTarefa.duedate, done: dataTarefa.done, hide: dataTarefa.hide })
+            return res.json(tarefas)
         }
         return res.json(dataTarefa)
-    })
-}
+    }
+
 
 }
 export default new TarefaController()
